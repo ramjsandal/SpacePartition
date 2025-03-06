5a79d3c1b3da69d11c1e331db0b5a988334e1fcb
ramjsandal <61168190+ramjsandal@users.noreply.github.com>
2025-03-06 13:51:02 -0500

    added main proj

---
+++ SpacePartitionVisualization/Program.cs
@@ -0,0 +1,38 @@
+﻿using Raylib_cs;
+using SpacePartition;
+
+
+public class Program
+{
+
+    public static void Main()
+    {
+        SpacePartition.SpacePartition root = new SpacePartition.SpacePartition(0, 0, 800, 800, null);
+        root.Subdivide(0, 100, 100);
+        root.AssignTraversability();
+        root.PrintLeaves();
+        List<SpacePartition.SpacePartition> partitions = new List<SpacePartition.SpacePartition>();
+        root.GetAllLeaves(ref partitions);
+
+        int screenWidth = 800;
+        int screenHeight = 800;
+        Raylib.InitWindow(screenWidth, screenHeight, "Wahoo");
+
+        while (!Raylib.WindowShouldClose())
+        {
+
+            Raylib.ClearBackground(Color.SkyBlue);
+            Raylib.BeginDrawing();
+
+            foreach (SpacePartition.SpacePartition partition in partitions)
+            {
+                Raylib.DrawRectangle(partition.xOrigin, partition.yOrigin, partition.width, partition.height, partition.traversible ? Raylib_cs.Color.White : Raylib_cs.Color.Black);
+            }
+
+            Raylib.EndDrawing();
+        }
+
+        Raylib.CloseWindow();
+
+    }
+}
